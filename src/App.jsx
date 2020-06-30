49729fe44eeb0dcdbd4cf5432875707e31f22eab
Abtahee Ali <abtaheeali@gmail.com>
2020-06-30 15:29:32 -0400

    changed class to className

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,28 @@ function App() {
   return (
     <body>
       <main>
-        <div class="calculator">
-          <div class="display">0</div>
-          <div class="buttons">
-            <button class="button c">AC</button>
-            <button class="button fn">&#177;</button>
-            <button class="button fn">&#37;</button>
-            <button class="button op">&#247;</button>
-            <button class="button">7</button>
-            <button class="button">8</button>
-            <button class="button">9</button>
-            <button class="button op">&#215;</button>
-            <button class="button">4</button>
-            <button class="button">5</button>
-            <button class="button">6</button>
-            <button class="button op">&#8722;</button>
-            <button class="button">1</button>
-            <button class="button">2</button>
-            <button class="button">3</button>
-            <button class="button op">&#43;</button>
-            <button class="button x2">0</button>
-            <button class="button">.</button>
-            <button class="button op">&#61;</button>
+        <div className="calculator">
+          <div className="display">0</div>
+          <div className="buttons">
+            <button className="button c">AC</button>
+            <button className="button fn">&#177;</button>
+            <button className="button fn">&#37;</button>
+            <button className="button op">&#247;</button>
+            <button className="button">7</button>
+            <button className="button">8</button>
+            <button className="button">9</button>
+            <button className="button op">&#215;</button>
+            <button className="button">4</button>
+            <button className="button">5</button>
+            <button className="button">6</button>
+            <button className="button op">&#8722;</button>
+            <button className="button">1</button>
+            <button className="button">2</button>
+            <button className="button">3</button>
+            <button className="button op">&#43;</button>
+            <button className="button x2">0</button>
+            <button className="button">.</button>
+            <button className="button op">&#61;</button>
           </div>
         </div>
       </main>
