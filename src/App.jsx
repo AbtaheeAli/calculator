23817290025897f85280d72e16ba4a08173cca2a
Abtahee Ali <abtaheeali@gmail.com>
2020-06-30 15:26:10 -0400

    Added html and css for calc

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
-  return
+  return (
+    <body>
+      <main>
+        <div class="calculator">
+          <div class="display">0</div>
+          <div class="buttons">
+            <button class="button c">AC</button>
+            <button class="button fn">&#177;</button>
+            <button class="button fn">&#37;</button>
+            <button class="button op">&#247;</button>
+            <button class="button">7</button>
+            <button class="button">8</button>
+            <button class="button">9</button>
+            <button class="button op">&#215;</button>
+            <button class="button">4</button>
+            <button class="button">5</button>
+            <button class="button">6</button>
+            <button class="button op">&#8722;</button>
+            <button class="button">1</button>
+            <button class="button">2</button>
+            <button class="button">3</button>
+            <button class="button op">&#43;</button>
+            <button class="button x2">0</button>
+            <button class="button">.</button>
+            <button class="button op">&#61;</button>
+          </div>
+        </div>
+      </main>
+    </body>
+  )
 }
 
 export default App
